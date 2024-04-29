530b883c84118cf424887142028eb190fd6aa917
Nate Schieber <nbschieber@gmail.com>
2024-04-29 13:05:18 -0700

    basic akka running

--- gs_desktop_application/src/main/scala/gs_desktop_application/App.scala
+++ gs_desktop_application/src/main/scala/gs_desktop_application/App.scala
@@ -1,10 +1,31 @@
 package gs_desktop_application
 
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.Route
+import scala.concurrent.ExecutionContext.Implicits.global
+
 object App {
 
   def simonSays(x : Array[String]): String = x.foldLeft("Simon Says: ")((a,b) => a + " " + b)
 
   @main def main(): Unit =
+
+    given system: ActorSystem = ActorSystem("gs-desktop-app")
+    val route = path("foo") {
+      get {
+        complete("Hello World! Your friend, Akka. ")
+      }
+    }
+    val server = Http().newServerAt("localhost", 8081).bind(route)
+    server.map { _ =>
+      println("Server online at http://localhost:8081")
+    } recover { case ex =>
+      println(s"Server could not start: ${ex.getMessage}")
+    }
+
+
     val instructions: Array[String] = Array("How", "now,", "brown", "cow?")
     println(simonSays(instructions))
 
