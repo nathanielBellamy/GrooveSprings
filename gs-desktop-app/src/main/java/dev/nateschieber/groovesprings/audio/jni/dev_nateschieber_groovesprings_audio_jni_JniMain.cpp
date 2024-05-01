11fc88662c6d112405ed180fce49e1bbae619f5f
Nate Schieber <nbschieber@gmail.com>
2024-05-01 06:17:13 -0700

    remove gs-desktop-app

--- gs-desktop-app/src/main/java/dev/nateschieber/groovesprings/audio/jni/dev_nateschieber_groovesprings_audio_jni_JniMain.cpp
+++
@@ -1,8 +0,0 @@
-#include "jni.h"
-#include<iostream>
-#include "dev_nateschieber_groovesprings_audio_jni_JniMain.h"
-
-JNIEXPORT void JNICALL Java_dev_nateschieber_groovesprings_audio_jni_JniMain_helloWorld
-  (JNIEnv* env, jobject thisObject) {
-    std::cout << "Hello, Java! Your pal, C++ !!" << std::endl;
-}
