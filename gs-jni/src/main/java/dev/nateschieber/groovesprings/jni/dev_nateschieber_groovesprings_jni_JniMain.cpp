5eb7d58af5b1db72a79c9ccc187c53151a847e3c
eta_ote <eta_ote@eta-otes-MacBook-Air.local>
2024-05-03 10:36:17 -0700

    run it on macos

--- gs-jni/src/main/java/dev/nateschieber/groovesprings/jni/dev_nateschieber_groovesprings_jni_JniMain.cpp
+++ gs-jni/src/main/java/dev/nateschieber/groovesprings/jni/dev_nateschieber_groovesprings_jni_JniMain.cpp
@@ -2,6 +2,12 @@
 #include<iostream>
 #include "dev_nateschieber_groovesprings_jni_JniMain.h"
 
+// NOTE:
+//   - on macOs, .dylib compilation requires main method
+int main() {
+    return 0;
+}
+
 JNIEXPORT void JNICALL Java_dev_nateschieber_groovesprings_jni_JniMain_helloWorld
   (JNIEnv* env, jobject thisObject) {
     std::cout << "Hey, Java! Tell Scala that C++ says hi !!" << std::endl;
