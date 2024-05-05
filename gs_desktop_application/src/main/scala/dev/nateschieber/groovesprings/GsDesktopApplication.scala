1f0c2da08eebb26bf422d381c7c2455fdf6f74cf
eta_ote <eta_ote@eta-otes-MacBook-Air.local>
2024-05-05 13:55:08 -0700

    scaffolding actors

--- gs_desktop_application/src/main/scala/dev/nateschieber/groovesprings/GsDesktopApplication.scala
+++ gs_desktop_application/src/main/scala/dev/nateschieber/groovesprings/GsDesktopApplication.scala
@@ -1,9 +1,11 @@
 package dev.nateschieber.groovesprings
 
-import akka.actor.ActorSystem
+import akka.actor.typed.ActorSystem
+import akka.actor.Props
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.server.Directives.*
 import akka.http.scaladsl.server.Route
+import dev.nateschieber.groovesprings.actors.GsSupervisor
 import dev.nateschieber.groovesprings.jni.JniMain
 
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -12,7 +14,7 @@ object GsDesktopApplication {
 
   @main def main(): Unit = {
     println(JniMain.main())
-    given system: ActorSystem = ActorSystem("gs_desktop_application")
+    given system: ActorSystem[Nothing] = ActorSystem(GsSupervisor(), "gs_desktop_application")
 
     lazy val server = Http().newServerAt("localhost", 8765).bind(routes())
 
