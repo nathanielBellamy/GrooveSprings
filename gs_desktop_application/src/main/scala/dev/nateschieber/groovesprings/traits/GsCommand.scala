4f6f52a5d758357d20b4e12986c83e372cfbd521
Nate Schieber <nbschieber@gmail.com>
2024-05-06 19:04:46 -0700

    sketch GsTransportControl

--- gs_desktop_application/src/main/scala/dev/nateschieber/groovesprings/traits/GsCommand.scala
+++ gs_desktop_application/src/main/scala/dev/nateschieber/groovesprings/traits/GsCommand.scala
@@ -4,8 +4,23 @@ import akka.actor.typed.ActorRef
 
 sealed trait GsCommand
 
+
+// GsDisplay
+final case class InitDisplay(playbackRef: ActorRef[ReadFrameId]) extends GsCommand
+
+// GsPlayback
 final case class ReadFrameId(replyTo: ActorRef[RespondFrameId]) extends GsCommand
 final case class RespondFrameId(value: Integer, replyTo: ActorRef[ReadFrameId]) extends GsCommand
 
-final case class InitDisplay(playbackRef: ActorRef[ReadFrameId]) extends GsCommand
+final case class RespondPlayTrig(replyTo: ActorRef[PlayTrig]) extends GsCommand
+final case class RespondPauseTrig(replyTo: ActorRef[PauseTrig]) extends GsCommand
+final case class RespondStopTrig(replyTo: ActorRef[StopTrig]) extends GsCommand
+final case class RespondFastForwardTrig(replyTo: ActorRef[FastForwardTrig]) extends GsCommand
+final case class RespondRewindTrig(replyTo: ActorRef[RewindTrig]) extends GsCommand
 
+// GsTransportControl
+final case class PlayTrig(replyTo: ActorRef[RespondPlayTrig]) extends GsCommand
+final case class PauseTrig(replyTo: ActorRef[RespondPauseTrig]) extends GsCommand
+final case class StopTrig(replyTo: ActorRef[RespondStopTrig]) extends GsCommand
+final case class FastForwardTrig(replyTo: ActorRef[RespondFastForwardTrig]) extends GsCommand
+final case class RewindTrig(replyTo: ActorRef[RespondRewindTrig]) extends GsCommand
