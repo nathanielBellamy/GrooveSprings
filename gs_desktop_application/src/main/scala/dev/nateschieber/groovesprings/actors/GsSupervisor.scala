c4ec4688524b72b0cae27728ad778098073eb114
nateschieber <nbschieber@gmail.com>
2024-05-19 16:01:53 -0700

    scaffold gsRestController

--- gs_desktop_application/src/main/scala/dev/nateschieber/groovesprings/actors/GsSupervisor.scala
+++ gs_desktop_application/src/main/scala/dev/nateschieber/groovesprings/actors/GsSupervisor.scala
@@ -1,18 +1,17 @@
 package dev.nateschieber.groovesprings.actors
 
-import akka.actor.typed.Behavior
-import akka.actor.typed.PostStop
-import akka.actor.typed.Signal
+import akka.actor.typed.{Behavior, PostStop, Signal}
 import akka.actor.typed.scaladsl.AbstractBehavior
 import akka.actor.typed.scaladsl.ActorContext
 import akka.actor.typed.scaladsl.Behaviors
-import dev.nateschieber.groovesprings.traits.{InitDisplay, PlayTrig, StopTrig}
+import dev.nateschieber.groovesprings.traits.InitDisplay
 
 object GsSupervisor {
   def apply(): Behavior[Nothing] = Behaviors.setup {
     context =>
       val playbackRef = context.spawn(GsPlayback(), "gs_playback")
       val displayRef = context.spawn(GsDisplay(playbackRef), "gs_display")
+      val restControllerRef = context.spawn(GsRestController(playbackRef), "gs_rest_controller")
       val transportControlRef = context.spawn(GsTransportControl(playbackRef, displayRef), "gs_transport_control")
 
       displayRef ! InitDisplay()
--- gs_desktop_application/src/main/scala/dev/nateschieber/groovesprings/enums/GsHttpPort.scala
+++ gs_desktop_application/src/main/scala/dev/nateschieber/groovesprings/enums/GsHttpPort.scala
@@ -3,5 +3,6 @@ package dev.nateschieber.groovesprings.enums
 enum GsHttpPort(number: Int):
   def port = number
 
-  case GsDesktopApplication extends GsHttpPort(8765)
+  case GsDesktopApplication extends GsHttpPort(5678)
+  case GsRestController extends GsHttpPort(8765)
   case GsTransportControl extends GsHttpPort(8766)
