e2c98207f81e5716641559b03f963eb7cba4ec26
nateschieber <nbschieber@gmail.com>
2024-06-12 18:35:54 -0700

    scaffold gs-music-library-scanner

---
+++ gs-music-library-scanner/src/main/java/dev/nateschieber/groovesprings/GsMusicLibraryScanner.java
@@ -0,0 +1,9 @@
+package dev.nateschieber.groovesprings;
+
+public class GsMusicLibraryScanner {
+
+    public static void main(String[] args) {
+        System.out.println("======== GS MUSIC LIBRARY SCANNER");
+
+    }
+}
