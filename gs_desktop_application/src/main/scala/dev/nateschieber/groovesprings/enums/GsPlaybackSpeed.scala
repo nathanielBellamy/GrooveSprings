9221d9bf7902dee58ff3698810588e33fabff914
nateschieber <nbschieber@gmail.com>
2024-06-12 18:00:58 -0700

    all playback speeds for now

--- gs_desktop_application/src/main/scala/dev/nateschieber/groovesprings/enums/GsPlaybackSpeed.scala
+++ gs_desktop_application/src/main/scala/dev/nateschieber/groovesprings/enums/GsPlaybackSpeed.scala
@@ -7,9 +7,12 @@ import scala.annotation.static
 enum GsPlaybackSpeed(valueIn: Float):
   def value = valueIn
 
-  case _1  extends GsPlaybackSpeed(1.0)
-  case _05 extends GsPlaybackSpeed(0.5) // half-speed
-  case _N1 extends GsPlaybackSpeed(-1.0)
+  case _2   extends GsPlaybackSpeed(2.0)  // double speed
+  case _1   extends GsPlaybackSpeed(1.0)  // normal playback
+  case _05  extends GsPlaybackSpeed(0.5)  // half-speed
+  case _N05 extends GsPlaybackSpeed(-0.5) // half-speed reverse
+  case _N1  extends GsPlaybackSpeed(-1.0) // reverse
+  case _N2  extends GsPlaybackSpeed(-2.0) // double speed reverse
 
 //  case _1  extends GsPlaybackSpeed(1)
 //  case _2  extends GsPlaybackSpeed(2)
