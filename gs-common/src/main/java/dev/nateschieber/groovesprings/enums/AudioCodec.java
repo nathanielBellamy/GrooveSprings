7174e79ae2b9cb0fdca1328773c9532cfe6bd35f
nateschieber <nbschieber@gmail.com>
2024-06-12 19:07:02 -0700

    more scaffolding gs-music-library-scanner

--- gs-common/src/main/java/dev/nateschieber/groovesprings/enums/AudioCodec.java
+++ gs-common/src/main/java/dev/nateschieber/groovesprings/enums/AudioCodec.java
@@ -1,19 +1,21 @@
 package dev.nateschieber.groovesprings.enums;
 
 public enum AudioCodec {
-  WAV(1000),
-  ALAC(900),
-  AAC(800),
-  FLAC(700),
-  OGG(600),
-  MP3_320(500),
-  MP3_256(400),
-  MP3_192(300),
-  MP3_VAR(200);
+  WAV("wav", 1000),
+  ALAC("alac", 900),
+  AAC("aac", 800),
+  FLAC("flac", 700),
+  OGG("ogg", 600),
+  MP3_320("mp3", 500),
+  MP3_256("mp3", 400),
+  MP3_192("mp3", 300),
+  MP3_VAR("mp3", 200);
 
+  private String fileExtension;
   private int priceFactor;
 
-  AudioCodec(int priceFactor) {
+  AudioCodec(String fileExtension, int priceFactor) {
+    this.fileExtension = fileExtension;
     this.priceFactor = priceFactor;
   }
 
@@ -21,7 +23,8 @@ public enum AudioCodec {
     return priceFactor;
   }
 
-  public void setPriceFactor(int priceFactor) {
-    this.priceFactor = priceFactor;
+  public String getFileExtension() {
+    return fileExtension;
   }
+
 }
