a3a35cff94368226ae58321f39ee1c9aed8eb552
nateschieber <nbschieber@gmail.com>
2024-06-14 12:12:29 -0700

    clear playlist, clear playlist on lib clear

--- gs_desktop_application/src/main/frontend/angular/gs_desktop_frontend/src/app/store/app.effects.ts
+++ gs_desktop_application/src/main/frontend/angular/gs_desktop_frontend/src/app/store/app.effects.ts
@@ -1,6 +1,8 @@
 import {libraryEffects} from "../features/library/store/library.effects";
+import {PlaybackEffects} from "../features/playback/store/playback.effects";
 
 
 export const gsEffects = [
-  ...libraryEffects
+  ...libraryEffects,
+  PlaybackEffects
 ]
