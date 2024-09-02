19054203f401f67cda8850aebfeefa3a1ff83cb9
Nate Schieber <nbschieber@gmail.com>
2024-09-02 06:02:31 -0700

    wip: init audio client, todo: debug cmake detect platform

--- gs-jni/src/main/java/dev/nateschieber/groovesprings/jni/audio/audio_data.h
+++ gs-jni/src/main/java/dev/nateschieber/groovesprings/jni/audio/audio_data.h
@@ -2,21 +2,23 @@
 #define AUDIO_DATA_H
 #include <sndfile.hh>
 #include "./jni_data.h"
+#include "./effects/vst3/host/audiohost/source/audiohost.h"
 
 struct AUDIO_DATA {
-    sf_count_t  index;
-    float       *buffer;
-    SNDFILE     *file;
-    SF_INFO     sfinfo;
-    long        readcount;
-    int         playState;
-    float       playbackSpeed;
-    bool        readComplete;
-    float       volume;
-    float       fadeIn;
-    float       fadeOut;
+    sf_count_t      index;
+    float           *buffer;
+    SNDFILE         *file;
+    SF_INFO         sfinfo;
+    long            readcount;
+    int             playState;
+    float           playbackSpeed;
+    bool            readComplete;
+    float           volume;
+    float           fadeIn;
+    float           fadeOut;
+//    AudioClientPtr  vst3AudioClient;
 
-    AUDIO_DATA(float* buffer, SNDFILE* file, SF_INFO sfinfo, sf_count_t index, long readcount, int playState) :
+    AUDIO_DATA(float* buffer, SNDFILE* file, SF_INFO sfinfo, sf_count_t index, long readcount, int playState) : //, AudioClientPtr vst3AudioClientPtr) :
           buffer(buffer)
         , file(file)
         , sfinfo(sfinfo)
@@ -26,7 +28,9 @@ struct AUDIO_DATA {
         , readComplete(false)
         , volume(0.0)
         , fadeIn(1.0)
-        , fadeOut(1.0) {}
+        , fadeOut(1.0)
+//        , vst3AudioClientPtr(vst3AudioClientPtr)
+        {}
 };
 
 #endif
