04f69be3ebe085c1e60ac315ec2ee2c3c5170aad
Nate Schieber <nbschieber@gmail.com>
2024-09-07 23:12:08 -0700

    running Vst3HostApp in paralellel to audio threads, passing ptr thru with AUDIO_DATA

--- gs-jni/src/main/java/dev/nateschieber/groovesprings/jni/JniMain.java
+++ gs-jni/src/main/java/dev/nateschieber/groovesprings/jni/JniMain.java
@@ -18,12 +18,13 @@ public class JniMain {
 
   private native int addNative(int x, int y);
 
-  public static void initPlaybackLoop(Long threadId, String filePath, Long initialFrameId) {
+  public static void initPlaybackLoop(Long threadId, String filePath, Long initialFrameId, Long vst3HostAppPtr) {
     long initFrameId = initialFrameId == null ? 0l : initialFrameId.longValue();
-    new JniMain().initPlaybackLoopNative(threadId, filePath, initialFrameId);
+    long vst3Ptr = vst3HostAppPtr == null ? 0l : initialFrameId.longValue();
+    new JniMain().initPlaybackLoopNative(threadId, filePath, initFrameId, vst3Ptr);
   }
 
-  private native void initPlaybackLoopNative(long threadId, String filePath, long initialFrameId); // blocking
+  private native void initPlaybackLoopNative(long threadId, String filePath, long initialFrameId, long vst3HostAppPtr); // blocking
 
   public static SfInfo readSfInfo(String filePath) {
     SfInfo res = new JniMain().readSfInfoNative(filePath);
@@ -32,15 +33,21 @@ public class JniMain {
 
   private native SfInfo readSfInfoNative(String filePath);
 
-  public static Vst3AudioHostAppPtr allocVst3Host(Vst3AudioHostAppPtr ptr) {
-    return (Vst3AudioHostAppPtr) new JniMain().allocVst3HostNative((Object) ptr);
+  public static long allocVst3Host() {
+    return new JniMain().allocVst3HostNative();
   };
 
-  private native Object allocVst3HostNative(Object ptr);
+  private native long allocVst3HostNative();
 
-  public static void initVst3Host(Vst3AudioHostAppPtr ptr) {
-    new JniMain().initVst3HostNative(ptr.getAddress());
+  public static long deleteVst3Host(long vst3HostAppPtr) {
+    return new JniMain().deleteVst3HostNative(vst3HostAppPtr);
+  };
+
+  private native long deleteVst3HostNative(long vst3HostAppPtr);
+
+  public static void initVst3Host(long appPtr) {
+    new JniMain().initVst3HostNative(appPtr);
   }
 
-  private native void initVst3HostNative(int addr); // blocking
+  private native void initVst3HostNative(long appPtr); // blocking
 }
--- gs_desktop_application/src/main/scala/dev/nateschieber/groovesprings/actors/GsVst3HostThread.scala
+++ gs_desktop_application/src/main/scala/dev/nateschieber/groovesprings/actors/GsVst3HostThread.scala
@@ -1,17 +1,25 @@
 package dev.nateschieber.groovesprings.actors
 
+import akka.Done
+import akka.actor.CoordinatedShutdown
 import akka.actor.typed.scaladsl.{AbstractBehavior, ActorContext, Behaviors}
 import akka.actor.typed.{ActorSystem, Behavior}
 import dev.nateschieber.groovesprings.actors.GsVst3HostThread.vst3HostAppPtr
 import dev.nateschieber.groovesprings.jni
-import dev.nateschieber.groovesprings.jni.{JniMain, Vst3AudioHostAppPtr}
+import dev.nateschieber.groovesprings.jni.JniMain
 import dev.nateschieber.groovesprings.traits.{GsCommand, InitVst3Host}
+import scala.concurrent.ExecutionContext.Implicits.global
 
 import scala.annotation.static
+import scala.concurrent.Future
 
 object GsVst3HostThread {
 
-  @static private var vst3HostAppPtr: Vst3AudioHostAppPtr = synchronized { null }
+  @static private var vst3HostAppPtr: Long = synchronized { 0l }
+
+  @static def getVst3HostPtr: Long = {
+    vst3HostAppPtr
+  }
 
   def apply(): Behavior[GsCommand] = Behaviors.setup {
     context =>
@@ -27,15 +35,21 @@ class GsVst3HostThread (context: ActorContext[GsCommand]) extends AbstractBehavi
     msg match {
 
       case InitVst3Host(replyTo) =>
-//        JniMain.allocVst3Host(vst3HostAppPtr)
-//        println("vst3HostAddr" + vst3HostAppPtr.getAddress)
-//        JniMain.initVst3Host(vst3HostAppPtr)
-        println("after after after")
+        vst3HostAppPtr = JniMain.allocVst3Host()
+        JniMain.initVst3Host(vst3HostAppPtr)
         Behaviors.same
 
       case default => 
         println("GsVst3HostThread Unmatched Message \n" + msg + "\n End GsVst3Host Unmatched Message \n")
         Behaviors.same
+    }
+  }
+
+
+  CoordinatedShutdown(context.system).addTask(CoordinatedShutdown.PhaseBeforeServiceUnbind, "cacheStateOnShutdown") { () =>
+    Future {
+      JniMain.deleteVst3Host(vst3HostAppPtr)
+      Done
     }
   }
 
