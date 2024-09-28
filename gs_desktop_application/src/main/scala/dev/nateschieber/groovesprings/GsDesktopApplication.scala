0d52fb088da4ff5cf625ee4ad40791acb6d319eb
Nate Schieber <nbschieber@gmail.com>
2024-09-28 01:01:23 -0700

    formatting

--- gs_desktop_application/src/main/scala/dev/nateschieber/groovesprings/GsDesktopApplication.scala
+++ gs_desktop_application/src/main/scala/dev/nateschieber/groovesprings/GsDesktopApplication.scala
@@ -10,32 +10,30 @@ import dev.nateschieber.groovesprings.jni.JniMain
 
 import java.awt.Desktop
 import java.net.URI
-import scala.annotation.static
 import scala.concurrent.ExecutionContext.Implicits.global
 
+@main def GsDesktopApplication(): Unit = {
+  val vst3HostAppPtr: Long = JniMain.allocVst3Host()
+  JniMain.initVst3Host(vst3HostAppPtr)
 
-  @main def GsDesktopApplication(): Unit = {
-    val vst3HostAppPtr: Long = JniMain.allocVst3Host()
-    JniMain.initVst3Host(vst3HostAppPtr)
+  given system: ActorSystem[Nothing] = ActorSystem(GsSupervisor(1l), "gs_desktop_application")
 
-    given system: ActorSystem[Nothing] = ActorSystem(GsSupervisor(1l), "gs_desktop_application")
+  lazy val server = Http().newServerAt("localhost", GsHttpPort.GsDesktopApplication.port).bind(routes())
 
-    lazy val server = Http().newServerAt("localhost", GsHttpPort.GsDesktopApplication.port).bind(routes())
+  server.map(_ => {
+    //
+  })
 
-    server.map(_ => {
-      //
-    })
+  if (Desktop.isDesktopSupported && Desktop.getDesktop.isSupported(Desktop.Action.BROWSE))
+    Desktop.getDesktop.browse(new URI("http://localhost:" + GsHttpPort.GsRestController.port))
+}
 
-    if (Desktop.isDesktopSupported && Desktop.getDesktop.isSupported(Desktop.Action.BROWSE))
-      Desktop.getDesktop.browse(new URI("http://localhost:" + GsHttpPort.GsRestController.port))
-  }
-
-  def routes(): Route = {
-    concat(
-      path("api" / "v1" / "hello") {
-        get {
-          complete("Welcome to Groove Springs.")
-        }
+def routes(): Route = {
+  concat(
+    path("api" / "v1" / "hello") {
+      get {
+        complete("Welcome to Groove Springs.")
       }
-    )
-  }
+    }
+  )
+}
