e8e0f70ddb7f6af11efaf187f1f7a9b69a3bfd2f
Nate Schieber <nbschieber@gmail.com>
2024-10-03 23:40:49 -0700

    hello caf

--- gs_desktop_application/src/main/cpp/main.h
+++ gs_desktop_application/src/main/cpp/main.h
@@ -1,13 +1,19 @@
 
-#include <jni.h>
-
 #ifndef MAIN_H
 #define MAIN_H
 
 #include <iostream>
 
+#include "caf/actor_ostream.hpp"
+#include "caf/actor_system.hpp"
+#include "caf/caf_main.hpp"
+#include "caf/event_based_actor.hpp"
+
+namespace GrooveSprings {
+
 extern "C" {
-    int main();
 }
+
+} // GrooveSprings
 
 #endif //MAIN_H
