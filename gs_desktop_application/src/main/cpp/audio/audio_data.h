3ca00065c7ee5b05c26d57829979cfe411d85394
Nate Schieber <nbschieber@gmail.com>
2024-10-12 11:31:52 -0700

    building audio decoupled from jvm, todo: much debug

---
+++ gs_desktop_application/src/main/cpp/audio/audio_data.h
@@ -0,0 +1,35 @@
+#ifndef AUDIO_DATA_H
+#define AUDIO_DATA_H
+#include <sndfile.hh>
+#include "./effects/vst3/host/audiohost/source/audiohost.h"
+
+struct AUDIO_DATA {
+    sf_count_t                      index;
+    float                           *buffer;
+    SNDFILE                         *file;
+    SF_INFO                         sfinfo;
+    long                            readcount;
+    int                             playState;
+    float                           playbackSpeed;
+    bool                            readComplete;
+    float                           volume;
+    float                           fadeIn;
+    float                           fadeOut;
+    long                            vst3HostPtr;
+
+    AUDIO_DATA(float* buffer, SNDFILE* file, SF_INFO sfinfo, sf_count_t index, long readcount, int playState, long vst3HostPtr) :
+          buffer(buffer)
+        , file(file)
+        , sfinfo(sfinfo)
+        , index(index)
+        , readcount(readcount)
+        , playState(playState)
+        , readComplete(false)
+        , volume(0.0)
+        , fadeIn(1.0)
+        , fadeOut(1.0)
+        , vst3HostPtr(vst3HostPtr)
+        {}
+};
+
+#endif
---
+++ gs_desktop_application/src/main/cpp/audio/sf_info_reader.h
@@ -0,0 +1,54 @@
+#ifndef SF_INFO_READER_H
+#define SF_INFO_READER_H
+
+struct SfInfoReader
+{
+    JNIEnv* env;
+    jclass jSfInfo;
+    jmethodID jSfInfoInit;
+    char const* filePath;
+    SF_INFO sf_info;
+
+    SfInfoReader(JNIEnv* env, jstring jFilePath):
+        env(env)
+        , jSfInfo(env->FindClass("dev/nateschieber/groovesprings/jni/SfInfo"))
+        , jSfInfoInit(env->GetMethodID(jSfInfo, "<init>", "(IIIIII)V"))
+        , filePath(env->GetStringUTFChars(jFilePath, 0)) {}
+
+    SF_INFO read() {
+      SF_INFO sf_info;
+      sf_open(SfInfoReader::filePath, SFM_READ, &sf_info);
+      return sf_info;
+    };
+
+    jobject jWrap(SF_INFO sf_info) {
+//       std::cout << "\n sf_info:::::::: \n";
+//       std::cout << "\n    " << SfInfoReader::filePath;
+//       std::cout << "\n    " << (int) sf_info.frames;
+//       std::cout << "\n    " << sf_info.samplerate;
+//       std::cout << "\n    " << sf_info.channels;
+//       std::cout << "\n    " << sf_info.format;
+//       std::cout << "\n    " << sf_info.sections;
+//       std::cout << "\n    " << sf_info.seekable;
+//       std::cout << "\n    " << SfInfoReader::jSfInfo;
+//       std::cout << "\n    " << SfInfoReader::jSfInfoInit;
+      jobject res = SfInfoReader::env->NewObject(
+        SfInfoReader::jSfInfo,
+        SfInfoReader::jSfInfoInit,
+         (int) sf_info.frames,
+         sf_info.samplerate,
+         sf_info.channels,
+         sf_info.format,
+         sf_info.sections,
+         sf_info.seekable
+      );
+
+      std::cout << "\n        " << res;
+      return res;
+    }
+};
+
+
+#endif
+
+
