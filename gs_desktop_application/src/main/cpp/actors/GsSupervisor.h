cf132647bc484bbc7155d4ebf76b3c7b2c5cbfb3
Nate Schieber <nbschieber@gmail.com>
2024-11-03 14:15:37 -0800

    scaffold gs_display

--- gs_desktop_application/src/main/cpp/actors/GsSupervisor.h
+++ gs_desktop_application/src/main/cpp/actors/GsSupervisor.h
@@ -12,30 +12,37 @@
 
 #include "../atoms.h"
 
+#include "./GsDisplay.h"
+
 using namespace caf;
 
 struct gs_supervisor_trait {
 
-    using signatures = type_list<result<int32_t>(add_a, int32_t, int32_t),
-                                 result<int32_t>(multiply_a, int32_t, int32_t)>;
+    using signatures = type_list<result<void>(bool)>;
 
 };
 
 using gs_supervisor = typed_actor<gs_supervisor_trait>;
 
 struct gs_supervisor_state {
-     int32_t c;
+     bool display;
      actor_system& sys;
 
      gs_supervisor::pointer self;
 
-     gs_supervisor_state(gs_supervisor::pointer self, actor_system& sys, int32_t c) :
+     gs_supervisor_state(gs_supervisor::pointer self, actor_system& sys) :
          self(self)
        , sys(sys)
-       , c(c)
+       , display(false)
          {
 //           auto gs_playback = sys.spawn(actor_from_state<gs_playback_state>);
-//           auto gs_display = sys.spawn(actor_from_state<gs_display_state>);
+           auto gs_display = sys.spawn(actor_from_state<gs_display_state>);
+           self->mail(init_display_a{})
+               .request(gs_display, infinite)
+               .then([&](bool success) {
+                     display = success;
+                     std::cout << "gs_supervisor_state::display = " << display << std::endl;
+                 });
 //           auto gs_app_state_manager = sys.spawn(actor_from_state<gs_app_state_manager_state>);
 //           auto gs_controller = sys.spawn(actor_from_state<gs_controller_state
 
@@ -43,11 +50,10 @@ struct gs_supervisor_state {
 
      gs_supervisor::behavior_type make_behavior() {
        return {
-           [this](add_a, int32_t a, int32_t b) {
-             std::cout << "wowza a: " << a << " b: " << b << std::endl;
-             return a + b + c;
+           [this](bool success) {
+             this->display = success;
+             std::cout << "gs_supervisor display: " << this->display << std::endl;
            },
-           [this](multiply_a, int32_t a, int32_t b) { return a * b * c; }
        };
      };
 };
